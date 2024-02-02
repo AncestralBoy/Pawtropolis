c327b3d78027cd49130c80f17b931549b497821c
AncestralBoy <100536878+AncestralBoy@users.noreply.github.com>
2024-02-02 22:57:33 +0100

    Change the call of the runGame metdo in GameController in the Main, using the interface CommandLineRunner

--- src/main/java/com/example/Pawtropolis/PawtropolisApplication.java
+++ src/main/java/com/example/Pawtropolis/PawtropolisApplication.java
@@ -1,17 +1,24 @@
 package com.example.Pawtropolis;
 
 import com.example.Pawtropolis.game.service.GameManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import org.springframework.context.support.AbstractApplicationContext;
 
 @SpringBootApplication
-public class PawtropolisApplication {
-
+@RequiredArgsConstructor
+public class PawtropolisApplication implements CommandLineRunner {
+	private final GameManager gameManager;
 	public static void main(String[] args) {
-		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(PawtropolisApplication.class);
-		ctx.getBean(GameManager.class).runGame();
-		ctx.close();
+		SpringApplication.run(PawtropolisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		gameManager.runGame();
 	}
 }
